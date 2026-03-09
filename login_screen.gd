811764ac6eabb10709489bb82c52ea52c52d5c6f
Griselda Guevara <griseldaguevara25@gmail.com>
2026-03-09 03:33:42 -0400

    Updated login screen/ menu

--- login_screen.gd
+++ login_screen.gd
@@ -1,8 +1,8 @@
-extends Control
+extends  Control
 
 @onready var username_input = $LoginBox/UserNameInput
 @onready var password_input = $LoginBox/PasswordInput
-@onready var message_label = $MessageLabel
+
 
 func _on_LoginButton_pressed():
 
@@ -10,7 +10,7 @@ func _on_LoginButton_pressed():
 		var password = password_input.text.strip_edges()
 
 		if username == "" or password == "":
-				message_label.text = "Please enter a username and password."
+				print("Please enter a username and password.")
 				return
 
 		get_tree().change_scene_to_file("res://menu.tscn")
@@ -19,3 +19,4 @@ func _on_LoginButton_pressed():
 func _on_GuestButton_pressed():
 
 		get_tree().change_scene_to_file("res://menu.tscn")
+		
