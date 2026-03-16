2028dd43b73bad5c36feded9c1c003b7045ffc30
Katie <kevans6@stetson.edu>
2026-03-16 15:01:40 -0400

    Working on NPC interactions

--- character_body_2d_npc.gd
+++ character_body_2d_npc.gd
@@ -10,12 +10,15 @@ func _ready():
 	$Area2D.body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body):
-	if body.is_class("CharacterBody2D"):
+	if body.name == "Player":
+		$CanvasLayer.visible = true
+		$AudioStreamPlayer.play()
+	
 		player_in_range = true
 		
 func _on_body_exited(body):
 	if body.name == "Player":
-		player_in_range = false
+		$CanvasLayer.visible = false
 
 func _process(delta):
 	if player_in_range and Input.is_action_just_pressed("ui_accept"):
